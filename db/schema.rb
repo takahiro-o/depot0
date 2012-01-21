0f0405227695bbd4c86b402d1251be67e428ce7c
takahiro-o <takahiro.o.1225@gmail.com>
2012-01-21 14:50:59 +0900

    depot120121

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(:version => 20120115123547) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
